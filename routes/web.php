9cc6cf3fa6e84df3cea07237de784c6a8224de63
Cristopher Ferrari <ferraricristopher93@gmail.com>
2024-02-27 16:37:41 +0100

    Creazione rotte e consegna

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+use App\Http\Controllers\AlbumController;
+use App\Http\Controllers\ArtistController;
 
-    return view('pages.index');
-});
+Route::get('/', [AlbumController :: class,  'index'])->name('album.index');
+Route::get('/album/show/{id}', [AlbumController :: class, 'show'])->name('album.show');
+Route::get('/artist/{id}', [ArtistController:: class, 'show'])->name('artist.show');
+Route::get('/album/create', [AlbumController :: class, 'create'])->name('album.create');
+Route::post('/album/create', [AlbumController :: class, 'store'])->name('album.store');
+Route::get('/album/{id}/edit', [AlbumController :: class, 'edit'])->name('album.edit');
+Route::put('album/{id}/edit', [AlbumController :: class, 'update'])->name('album.update');
+Route::delete('/album/{id}/delete', [AlbumController :: class, 'delete'])->name('album.delete');
